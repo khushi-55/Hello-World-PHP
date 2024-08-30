e5682a3f65f1cd74f587df9877d36a97253f5f6a
khushi-55 <asmkhushi6@gmail.com>
2024-08-30 08:36:40 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,1 @@
-<html>
- <head>
-  <title>PHP-Test</title>
- </head>
- <body>
-  <?php echo '<h1>Hello World!</h1><h3>Welcome to Intellipaat!</h3>'; ?>
- </body>
-</html>
+ <?php echo '<h1>Hello World!</h1><h3>Welcome to Intellipaat!</h3>'; ?>
